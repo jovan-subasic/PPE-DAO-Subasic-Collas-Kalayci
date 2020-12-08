e790d577f37fc3befe2443eae256d5ae70f14f3e
Julien <60462444+Jakyln@users.noreply.github.com>
2020-12-08 17:49:22 +0100

    ajout de bdd

--- PPE-DAO-S-C-K/PPE-DAO-S-C-K/DAO.Designer.cs
+++ PPE-DAO-S-C-K/PPE-DAO-S-C-K/DAO.Designer.cs
@@ -32,7 +32,16 @@
             this.tabPageAteliers = new System.Windows.Forms.TabPage();
             this.tabPageStand = new System.Windows.Forms.TabPage();
             this.tabPageInscription = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbx_ChoixAteliers = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
+            this.tabPageAteliers.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -48,10 +57,18 @@
             // 
             // tabPageAteliers
             // 
+            this.tabPageAteliers.Controls.Add(this.textBox3);
+            this.tabPageAteliers.Controls.Add(this.label4);
+            this.tabPageAteliers.Controls.Add(this.textBox2);
+            this.tabPageAteliers.Controls.Add(this.label3);
+            this.tabPageAteliers.Controls.Add(this.label2);
+            this.tabPageAteliers.Controls.Add(this.textBox1);
+            this.tabPageAteliers.Controls.Add(this.cbx_ChoixAteliers);
+            this.tabPageAteliers.Controls.Add(this.label1);
             this.tabPageAteliers.Location = new System.Drawing.Point(4, 22);
             this.tabPageAteliers.Name = "tabPageAteliers";
             this.tabPageAteliers.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageAteliers.Size = new System.Drawing.Size(387, 299);
+            this.tabPageAteliers.Size = new System.Drawing.Size(387, 303);
             this.tabPageAteliers.TabIndex = 0;
             this.tabPageAteliers.Text = "Ateliers";
             this.tabPageAteliers.UseVisualStyleBackColor = true;
@@ -71,10 +88,75 @@
             this.tabPageInscription.Location = new System.Drawing.Point(4, 22);
             this.tabPageInscription.Name = "tabPageInscription";
             this.tabPageInscription.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageInscription.Size = new System.Drawing.Size(387, 299);
+            this.tabPageInscription.Size = new System.Drawing.Size(387, 303);
             this.tabPageInscription.TabIndex = 2;
             this.tabPageInscription.Text = "Inscription";
             this.tabPageInscription.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Choix Des Ateliers";
+            // 
+            // cbx_ChoixAteliers
+            // 
+            this.cbx_ChoixAteliers.FormattingEnabled = true;
+            this.cbx_ChoixAteliers.Location = new System.Drawing.Point(135, 23);
+            this.cbx_ChoixAteliers.Name = "cbx_ChoixAteliers";
+            this.cbx_ChoixAteliers.Size = new System.Drawing.Size(121, 21);
+            this.cbx_ChoixAteliers.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Thème";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nom";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(135, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nombre de Places";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(135, 195);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
             // 
             // Maison_des_ligues
             // 
@@ -85,6 +167,8 @@
             this.Name = "Maison_des_ligues";
             this.Text = "Maison des ligues";
             this.tabControl1.ResumeLayout(false);
+            this.tabPageAteliers.ResumeLayout(false);
+            this.tabPageAteliers.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -95,6 +179,14 @@
         private System.Windows.Forms.TabPage tabPageAteliers;
         private System.Windows.Forms.TabPage tabPageStand;
         private System.Windows.Forms.TabPage tabPageInscription;
+        private System.Windows.Forms.ComboBox cbx_ChoixAteliers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
